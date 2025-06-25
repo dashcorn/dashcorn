c9065b4391b3af2d88419482188d47cdeb2b6a01
pnhung177 <pnhung177@gmail.com>
2025-06-26 05:44:08 +0700

    feat/prom_exporter: enhance the PrometheusHttpServer class

--- src/dashcorn/dashboard/prom_http_server.py
+++ src/dashcorn/dashboard/prom_http_server.py
@@ -2,20 +2,23 @@ import time
 import threading
 import logging
 
-from prometheus_client import start_http_server, REGISTRY
+from prometheus_client import REGISTRY, make_wsgi_app
+from wsgiref.simple_server import make_server
 
 from dashcorn.dashboard.prom_exporter import PrometheusExporter
 
 logger = logging.getLogger(__name__)
 
 class PrometheusHttpServer:
-    def __init__(self, state_provider, prom_port=9100):
+    def __init__(self, state_provider, prom_port=9100, prom_host=''):
         """
         state_provider: Callable trả về RealtimeState dict
         prom_port: cổng exporter Prometheus
         """
-        self.state_provider = state_provider
-        self.prom_port = prom_port
+        self._state_provider = state_provider
+        self._prom_host = prom_host
+        self._prom_port = prom_port
+        self._server = None
         self._thread = None
         self._stop_event = threading.Event()
         self._running = False
@@ -29,6 +32,7 @@ class PrometheusHttpServer:
         self._thread = threading.Thread(target=self._run_prometheus_exporter, daemon=True)
         self._thread.start()
         self._running = True
+        logger.debug(f"[{self.__class__.__name__}] started on port {self._prom_port}.")
 
     def stop(self):
         if not self._running:
@@ -36,6 +40,8 @@ class PrometheusHttpServer:
             return
 
         self._stop_event.set()
+        if self._server:
+            self._server.shutdown()
         self._thread.join(timeout=5)
         self._running = False
         logger.debug(f"[{self.__class__.__name__}] stopped.")
@@ -47,11 +53,10 @@ class PrometheusHttpServer:
 
     def _run_prometheus_exporter(self):
         try:
-            exporter = PrometheusExporter(self.state_provider)
+            exporter = PrometheusExporter(self._state_provider)
             REGISTRY.register(exporter)
-            start_http_server(self.prom_port)
-            logger.debug(f"[{self.__class__.__name__}] started on port {self.prom_port}.")
-            while not self._stop_event.is_set():
-                time.sleep(1)
+            app = make_wsgi_app(registry=REGISTRY)
+            self._server = make_server(self._prom_host, self._prom_port, app)
+            self._server.serve_forever()
         except Exception as e:
             logger.warning(f"[{self.__class__.__name__}] running error: {e}")
