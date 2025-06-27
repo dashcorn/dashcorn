a29d845e9a17288c1b8c7245d1314bdd90bd9c7a
pnhung177 <pnhung177@gmail.com>
2025-06-27 09:06:23 +0700

    feat/examples: update the demo.py

--- demo/demo.py
+++ demo/demo.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import FastAPI, Header
 
+from dashcorn.agent.config import AgentConfig
 from dashcorn.agent.middleware import MetricsMiddleware
 
 import dashcorn.utils.logging
@@ -11,3 +13,10 @@ app.add_middleware(MetricsMiddleware)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: str, x_request_id: Optional[str] = Header(None)):
+    return {
+        "id": user_id,
+        "x-request-id": x_request_id,
+    }
